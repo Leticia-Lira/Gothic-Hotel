b78bac9b16d8f8862733ae1c4fb17ea1b156ca86
Leticia-Lira <leticialiratattoo@gmail.com>
2024-05-22 11:16:10 -0300

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from "@/app/componentes/Topo/topo.module.css";
+
+export default function Topo(){
+    return(
+        
+      <div className={styles.topo_container}>
+      <h1>Gothic Hotel</h1>
+      <p>Dark&Cozy</p>
+      
+    </div>
+
+    );
+}
+
---
+++ rodape.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from "@/app/componentes/Rodapé/rodape.module.css";
+export default function Foot(){
+    return(
+        <div className={styles.rodape_container}>
+            <h4>Created by <a href="https://www.instagram.com/leticiaa__lira/" target='_blank'>Leticia Lira</a>, S.D</h4>
+            
+        </div>
+    );
+}
